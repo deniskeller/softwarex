93504f42eab11a3eca8de7fd23633761f85b906d
deniskeller <dentean@mail.ru>
2024-01-11 19:19:13 +0400

    add noise

--- src/components/landing/pages/main/Form/Form.tsx
+++ src/components/landing/pages/main/Form/Form.tsx
@@ -4,6 +4,7 @@ import { BaseButton, BaseContainer, BaseInput, BaseText } from '@base/index';
 import toast from 'react-hot-toast';
 import { Toast } from '@content/landing/index';
 import BaseInputPhone from '@base/BaseInputPhone/BaseInputPhone';
+import Image from 'next/image';
 
 interface IFormData {
   nameSurname: string;
@@ -36,17 +37,14 @@ const Form: React.FC = () => {
     <section className={s.Form}>
       <BaseContainer className={s.Form_Container}>
         <form action="" onSubmit={submitHandler}>
-          <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-            <filter id="noiseFilter">
-              <feTurbulence
-                type="fractalNoise"
-                baseFrequency="0.65"
-                numOctaves="3"
-                stitchTiles="stitch"
-              />
-            </filter>
-            <rect width="100%" height="100%" filter="url(#noiseFilter)" />
-          </svg>
+          <Image
+            src="/pictures/images/main/main-form-1920.png"
+            width={1099}
+            height={855}
+            alt=""
+            className={s.Form_Image}
+          />
+
           <BaseText className={s.Form_Title} as="h2">
             Say Hello
           </BaseText>
